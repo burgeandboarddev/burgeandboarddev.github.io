c933471d680d09be9e25981ba150bacfd1d73a72
burgeandboarddev <burgeandboarddev@gmail.com>
2023-12-13 21:36:04 -0600

    Add TODOs in README; cleanup config

--- README.md
+++ README.md
@@ -24,10 +24,14 @@
 ```
 
 ## TODO
+* [ ] Menu page
+* [ ] Monthly specials page
+* [ ] FAQ page
+* [ ] Order page
+* [ ] [Backend-less Google forms](https://medium.com/swlh/creating-a-backend-less-contact-form-using-google-forms-852157dcbdbb)
 * [x] Mobile Nav
 * [x] Social buttons
 * [x] Mobile homepage
 * [x] About page
 * [x] Fix github pages deployment
 * [x] [Deploy Next.js to GitHub Pages with GitHub Actions](https://github.com/gregrickaby/nextjs-github-pages)
-* [ ] [Backend-less Google forms](https://medium.com/swlh/creating-a-backend-less-contact-form-using-google-forms-852157dcbdbb)
--- next.config.js
+++ next.config.js
@@ -34,25 +34,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-// /**
-//  * https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-//  *
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: 'export',
-//
-//   // https://stackoverflow.com/a/68691247
-//   assetPrefix: './',
-//
-//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-//   // trailingSlash: true,
-//
-//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-//   // skipTrailingSlashRedirect: true,
-//
-//   // Convenient since GitHub already supports this.
-//   distDir: 'docs',
-// }
-//
-// module.exports = nextConfig
